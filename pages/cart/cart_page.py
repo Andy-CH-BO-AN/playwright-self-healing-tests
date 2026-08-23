bb88333f198217f40ba2a8fa10b999a6318564f2
chenboan <andypig9@gmail.com>
2026-08-23 23:57:47 +0800

    TEST: iterate auto heal

--- pages/cart/cart_page.py
+++ pages/cart/cart_page.py
@@ -5,7 +5,7 @@ class CartItem:
     def __init__(self, locator: Locator) -> None:
         self.root: Locator = locator
         self.name: Locator = locator.locator("[data-test='inventory-item-name']")
-        self.description: Locator = locator.locator("[data-test='inventory-item-desc']")
+        self.description: Locator = locator.locator("[data-test='inventory-item-description']")
         self.price: Locator = locator.locator("[data-test='inventory-item-price']")
         self.quantity: Locator = locator.locator("[data-test='item-quantity']")
         self.remove_button: Locator = locator.get_by_role("button", name="Remove")
